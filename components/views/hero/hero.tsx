4a003254bcbe050a547efef8153bca97b72f6f25
Irakli Begoidze <iraklibego1@gmail.com>
2026-02-17 21:36:36 +0400

    Minor hero background change, can be furthr improved

--- components/views/hero/hero.tsx
+++ components/views/hero/hero.tsx
@@ -40,7 +40,7 @@ function Hero({ avatarList }: HeroSectionProps) {
                         COLOR_UPDATE_SPEED={10}
                     />
                 </div>
-                <div className="relative w-full pt-0 md:pt-20 pb-6 md:pb-10 before:absolute before:w-full before:h-full before:bg-linear-to-r before:from-sky-100 before:via-white before:to-amber-100 before:rounded-full before:top-24 before:blur-3xl before:-z-10 dark:before:from-slate-800 dark:before:via-black dark:before:to-stone-700 dark:before:rounded-full dark:before:blur-3xl dark:before:-z-10">
+                <div className="relative w-full pt-0 md:pt-20 pb-6 md:pb-10 before:absolute before:w-full before:h-full before:bg-linear-to-r before:from-blue-300/80 before:via-white before:to-violet-200/80 before:rounded-full before:top-24 before:blur-3xl before:-z-10 dark:before:from-slate-800 dark:before:via-black dark:before:to-stone-700 dark:before:rounded-full dark:before:blur-3xl dark:before:-z-10">
                     <div className="container mx-auto relative z-10">
                         <div className="flex flex-col max-w-5xl mx-auto gap-8">
                             <div className="relative flex flex-col text-center items-center sm:gap-4 gap-2">
